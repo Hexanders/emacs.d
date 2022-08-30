4434f2744dda5fba312fbc4e0775ae776d620b86
Hexanders <alexander.a.kononov@gmail.com>
2022-08-30 16:56:25 +0200

    Some generall settings for emacs apperians and functionality

---
+++ init.el
@@ -0,0 +1,12 @@
+(setq inhibit-startup-message t 	;;turn off message 
+      visible-bell t			;;visual  
+      )
+
+(tool-bar-mode -1)			;;remove controll bar
+(scroll-bar-mode -1)			;;remove scroll bar
+(global-display-line-numbers-mode 1)	;;add line numbers to the screen
+(hl-line-mode 1)			;;highlight whole line
+(blink-cursor-mode 1)
+
+(load-theme 'modus-vivendi t)		;;dark theme
+
