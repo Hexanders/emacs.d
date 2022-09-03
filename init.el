7664e2054eccf9f6f29aa52e383e98840e40b75d
Hexanders <alexander.a.kononov@gmail.com>
2022-09-03 13:38:20 +0200

    add costom function for sudo-eddut from emacs. see reference

--- init.el
+++ init.el
@@ -54,6 +54,19 @@
 
 
 
+(defun sudo-edit (&optional arg) ;; http://emacsredux.com/blog/2013/04/21/edit-files-as-root/
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+
 ;;from hier some stuff wich was added by packag-list-packag
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
