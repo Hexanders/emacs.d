025725d654a6aaec1ace6f2185b8a9fe5b102997
Hexanders <alexander.a.kononov@gmail.com>
2022-09-07 10:15:35 +0200

    added auto comletion to latex added vertico: fiuzzy finder

--- init.el
+++ init.el
@@ -10,6 +10,52 @@
 (set-face-attribute 'default nil :height 135) ;;set font size to heigth/10 pt
 
 
+(require 'package) 
+
+(setq pacakge-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("elpa" : "https://elpa.gnu.org/packages/")))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+
+(require 'use-package)
+    (use-package vertico			;fuzzy finder
+		 :ensure t
+		 :init (vertico-mode))
+
+
+
+
+(require 'auto-complete-latex)
+(setq ac-modes (append ac-modes '(foo-mode)))
+(add-hook 'foo-mode-hook 'ac-l-setup)
+
+
+    
+;; auto-complete setup, sequence is important
+(require 'auto-complete)
+(add-to-list 'ac-modes 'latex-mode) ; beware of using 'LaTeX-mode instead
+(require 'ac-math) ; package should be installed first 
+(defun my-ac-latex-mode () ; add ac-sources for latex
+   (setq ac-sources
+         (append '(ac-source-math-unicode
+           ac-source-math-latex
+           ac-source-latex-commands)
+                 ac-sources)))
+(add-hook 'LaTeX-mode-hook 'my-ac-latex-mode)
+(setq ac-math-unicode-in-math-p t)
+(ac-flyspell-workaround) ; fixes a known bug of delay due to flyspell (if it is there)
+(add-to-list 'ac-modes 'org-mode) ; auto-complete for org-mode (optional)
+(require 'auto-complete-config) ; should be after add-to-list 'ac-modes and hooks
+(ac-config-default)
+(setq ac-auto-start nil)            ; if t starts ac at startup automatically
+(setq ac-auto-show-menu t)
+(global-auto-complete-mode t)
+
+
 
 (load "tabbar-tweak")			;tabbar config for tabs like in browser
 ;; (package-initialize)			;some older version og emacs requer this for (require 'use-package)
@@ -86,10 +132,15 @@ buffer is not visiting a file."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet tabbar lsp-java lsp-ltex lsp-mode lsp-python-ms lsp-ui auctex zotelo zotero))
+   '(use-package vertico yasnippet-snippets ac-math tabbar lsp-java lsp-ltex lsp-mode lsp-python-ms lsp-ui auctex zotelo zotero))
  '(tabbar-separator '(0.5))
  '(warning-suppress-log-types '((comp)))
- '(warning-suppress-types '((auto-save))))
+ '(warning-suppress-types
+   '((auto-save)
+     (auto-save)
+     (auto-save)
+     (auto-save)
+     (auto-save))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
